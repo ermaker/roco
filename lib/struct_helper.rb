4c3fa04a2658ae8f59052ac0fce67d3d4e792db0
Minwoo Lee <ermaker@gmail.com>
2010-10-13 22:13:10 +0900

    Add StructHelper.info

--- lib/struct_helper.rb
+++ lib/struct_helper.rb
@@ -142,4 +142,35 @@ module StructHelper
     end
   end
 
+  def all_possible_filenames
+    include_dirs.product(headers).map do |dir,file|
+      File.join(dir,file)
+    end
+  end
+  private :all_possible_filenames
+
+  # Get all struct information
+  def info
+    all_items = all_possible_filenames.map do |filename|
+      `ctags --fields=-ft+kz -f- "#{filename}"`.split("\n").map do |line|
+        items = line.split("\t")
+        [items[0], *items.select do |item|
+          item =~ /^kind:/ or item =~ /^struct:/
+        end]
+      end
+    end.flatten(1)
+    all_structs = all_items.select {|item| item.include?('kind:s')}
+    all_members = all_items.select {|item| item.include?('kind:m')}
+
+    result = Hash[all_structs.map do |struct|
+      [struct[0],[]]
+    end]
+
+    all_members.each do |member|
+      if member.any?{|item| item =~ /^struct:(.*)$/}
+        result[$1] << [typeof($1,member[0]), member[0]]
+      end
+    end
+    result 
+  end
 end
--- spec/struct_helper_spec.rb
+++ spec/struct_helper_spec.rb
@@ -37,4 +37,13 @@ describe StructHelper do
     StructHelper.typeof('simple_struct_char_array', 'simple_char_array').should == 'char [0]'
     StructHelper.typeof('simple_struct_char_array', 'simple_char_array[0]').should == 'char'
   end
+
+  it 'should get all struct infomation' do
+    StructHelper.info.sort.should == [
+      ["simple_struct_char_array", [["char [0]", "simple_char_array"]]],
+      ["simple_struct_int", [["int", "simple_int"]]],
+      ["simple_struct_int2", [["int", "simple_int1"], ["int", "simple_int2"]]],
+      ["simple_struct_int_array", [["int [3]", "simple_int_array"]]]
+    ].sort
+  end
 end
