41b13af1f79e1cc96d041e58eb43f9bef39b419e
Minwoo Lee <ermaker@gmail.com>
2010-10-14 01:42:32 +0900

    Fix feature

--- features/step_definitions/struct_helper_steps.rb
+++ features/step_definitions/struct_helper_steps.rb
@@ -6,7 +6,7 @@ Then /^all struct information is correct\.$/ do
   StructHelper.info.sort.should == [
     ["simple_struct_char_array", [0, [0, "char [0]", "simple_char_array"]]],
     ["simple_struct_int", [4, [0, "int", "simple_int"]]],
-    ["simple_struct_int2", [4, [0, "int", "simple_int1"], [4, "int", "simple_int2"]]],
+    ["simple_struct_int2", [8, [0, "int", "simple_int1"], [4, "int", "simple_int2"]]],
     ["simple_struct_int_array", [12, [0, "int [3]", "simple_int_array"]]]
   ].sort
 end
