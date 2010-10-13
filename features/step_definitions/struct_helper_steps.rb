0d9cb50f8e5c53d2406359df866b9ba895ae4490
Minwoo Lee <ermaker@gmail.com>
2010-10-13 22:17:47 +0900

    Modified getting struct information feature

--- features/step_definitions/struct_helper_steps.rb
+++ features/step_definitions/struct_helper_steps.rb
@@ -1,5 +1,12 @@
 require 'struct_helper'
 
-Then /^struct information of (\w+) is (.*).$/ do |name,expected|
-  StructHelper.info(name).to_s.should == expected
+Then /^all struct information is correct\.$/ do
+  StructHelper.include_dirs = ['spec/fixtures']
+  StructHelper.headers = %w[simple_struct.h]
+  StructHelper.info.sort.should == [
+    ["simple_struct_char_array", [["char [0]", "simple_char_array"]]],
+    ["simple_struct_int", [["int", "simple_int"]]],
+    ["simple_struct_int2", [["int", "simple_int1"], ["int", "simple_int2"]]],
+    ["simple_struct_int_array", [["int [3]", "simple_int_array"]]]
+  ].sort
 end
--- features/struct_info.feature
+++ features/struct_info.feature
@@ -1,4 +1,4 @@
 Feature: Get struct information
 
   Scenario:
-    * struct information of simple_struct_1 is .
+    * all struct information is correct.
