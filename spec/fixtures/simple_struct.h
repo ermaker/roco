bc4b697adc5cde8f1619ae5a5018b21d05754cb7
Minwoo Lee <ermaker@gmail.com>
2010-10-13 19:50:32 +0900

    Add methods of StructHelper

---
+++ spec/fixtures/simple_struct.h
@@ -0,0 +1,15 @@
+struct simple_struct_int
+{
+  int simple_int;
+};
+
+struct simple_struct_int2
+{
+  int simple_int1;
+  int simple_int2;
+};
+
+struct simple_struct_int_array
+{
+  int simple_int_array[3];
+};
--- spec/struct_helper_spec.rb
+++ spec/struct_helper_spec.rb
@@ -1,4 +1,27 @@
 require 'struct_helper'
 
 describe StructHelper do
+  before do
+    StructHelper.include_dirs = [File.dirname(__FILE__) + '/fixtures']
+    StructHelper.headers = %w[simple_struct.h]
+  end
+
+  it 'should get sizeof struct' do
+    StructHelper.sizeof('simple_struct_int').should == 4
+    StructHelper.sizeof('simple_struct_int2').should == 8
+    StructHelper.sizeof('simple_struct_int_array').should == 12
+  end
+
+  it 'should get offsetof member of struct' do
+    StructHelper.offsetof('simple_struct_int', 'simple_int').should == 0
+    StructHelper.offsetof('simple_struct_int2', 'simple_int1').should == 0
+    StructHelper.offsetof('simple_struct_int2', 'simple_int2').should == 4
+    StructHelper.offsetof('simple_struct_int_array', 'simple_int_array[0]').should == 0
+    StructHelper.offsetof('simple_struct_int_array', 'simple_int_array[1]').should == 4
+    StructHelper.offsetof('simple_struct_int_array', 'simple_int_array[2]').should == 8
+  end
+
+  it 'should get countof member of struct' do
+    StructHelper.countof('simple_struct_int_array', 'simple_int_array').should == 3
+  end
 end
