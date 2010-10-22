83b4c93468936ed6c6788cadf768dc598ed6492f
Minwoo Lee <ermaker@gmail.com>
2010-10-22 15:11:34 +0900

    Refactory

--- lib/cstruct.rb
+++ lib/cstruct.rb
@@ -20,14 +20,28 @@ module CStruct
   def length
     @length ||= File.size(filename) / sizeof_struct
   end
-
   alias size length
 
-  def each
+  def read_range range, &blk
+    first = correct_index range.first
+    last = correct_index range.last
+
+    return nil unless 0 <= first and first < length
+
+    new_range = range.exclude_end? ? first...last : first..last
+
     open(filename) do |f|
-      while data = f.read(sizeof_struct)
-        yield StructReader.make_struct(structname, data)
+      f.seek(new_range.first*sizeof_struct, IO::SEEK_SET)
+      blk.call(new_range) do
+        StructReader.make_struct(structname, f.read(sizeof_struct))
       end
+    end
+  end
+  private :read_range
+
+  def each
+    read_range((0..-1)) do |range,&blk|
+      range.each {yield blk[]}
     end
   end
 
@@ -41,21 +55,14 @@ module CStruct
 
   def [] idx_or_range
     if idx_or_range.class == Range
-      range = idx_or_range
-      first = correct_index range.first
-      last = correct_index range.last
-      new_range = range.exclude_end? ? first...last : first..last
-      new_range.map do |idx|
-        self[idx]
+      read_range(idx_or_range) do |range,&blk|
+        range.map {blk[]}
       end
     else
       idx = idx_or_range
       idx = correct_index idx
       if 0 <= idx && idx < length
-        open(filename) do |f|
-          f.seek(idx*sizeof_struct, IO::SEEK_SET)
-          StructReader.make_struct(structname, f.read(sizeof_struct))
-        end
+        self[idx..idx][0]
       end
     end
   end
