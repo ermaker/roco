67c344bd3f8b96193a94cea94c3570e9b6f570bb
Minwoo Lee <ermaker@gmail.com>
2010-12-19 05:01:34 +0900

    Add CStruct::Type::ArrayType

--- lib/cstruct/type.rb
+++ lib/cstruct/type.rb
@@ -1,5 +1,21 @@
 module CStruct
   module Type
+    class ArrayType
+      def initialize info, io
+        @info = info
+        @io = io
+        @offset = io.tell
+      end
+      def get index
+        @io.seek(@offset + @info[:type][:size] * index)
+        buf = @io.read(@info[:type][:size])
+        buf.unpack(@info[:type][:pack])[0]
+      end
+      def set index, value
+        @io.seek(@offset + @info[:type][:size] * index)
+        @io.write([value].pack(@info[:type][:pack]))
+      end
+    end
     class StructType
       def initialize info, io
         @info = info
@@ -9,7 +25,11 @@ module CStruct
       def get var_name
         var_info = @info[:member][var_name]
         @io.seek(@offset + var_info[:offset])
-        @io.read(var_info[:type][:size]).unpack(var_info[:type][:pack])[0]
+        if var_info[:count]
+          return ArrayType.new var_info, @io
+        end
+        buf = @io.read(var_info[:type][:size])
+        buf.unpack(var_info[:type][:pack])[0]
       end
       def set var_name, value
         var_info = @info[:member][var_name]
--- spec/cstruct/type_spec.rb
+++ spec/cstruct/type_spec.rb
@@ -13,4 +13,18 @@ describe CStruct::Type::StructType do
     io.seek(0)
     io.read.should == "\x02\x00\x00\x00"
   end
+  it 'should have getters and setters with a int array type member variable' do
+    info = YAML::load(open(File.dirname(__FILE__) +
+                           '/../fixtures/structures.yml'))
+    io = StringIO.new("\x01\x00\x00\x00\x02\x00\x00\x00\x03\x00\x00\x00")
+    struct = CStruct::Type::StructType.new(info["simple_struct_int_array"], io)
+    struct.get('simple_int_array').get(0).should == 1
+    struct.get('simple_int_array').get(1).should == 2
+    struct.get('simple_int_array').get(2).should == 3
+    struct.get('simple_int_array').set(0, 2)
+    struct.get('simple_int_array').set(1, 4)
+    struct.get('simple_int_array').set(2, 6)
+    io.seek(0)
+    io.read.should == "\x02\x00\x00\x00\x04\x00\x00\x00\x06\x00\x00\x00"
+  end
 end
