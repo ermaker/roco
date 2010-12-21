56f5e1715dbf611e7c99d63dabfff19428b088a0
Minwoo Lee <ermaker@gmail.com>
2010-12-21 18:45:17 +0900

    Refactor

--- spec/loco_spec.rb
+++ spec/loco_spec.rb
@@ -9,6 +9,7 @@ describe Loco do
     CStruct::Crawler.save('structures.yml')
     Loco::path = '../loco'
   end
+
   it 'should check login' do
     loco = Loco.new
     loco.login('SYSOP','1234').should == true
@@ -17,54 +18,37 @@ describe Loco do
     loco.login('user1','4321').should == true
     loco.login('user2','1234').should == true
   end
-end
 
-describe Loco::Userec do
-  before do
-    CStruct.include_dirs=['../loco']
-    CStruct.headers=['bbs.h']
-    CStruct::Crawler.save('structures.yml')
-    Loco::path = '../loco'
-  end
-  it 'should have userec class' do
-    Loco::Userec.as do |u|
-      u.login('SYSOP','1234').should == true
-      u.login('SYSOP','4321').should == false
-      u.login('SYSOP2','1234').should == false
-      u.login('user1','4321').should == true
-      u.login('user2','1234').should == true
+  describe Loco::Userec do
+    it 'should have userec class' do
+      Loco::Userec.as do |u|
+        u.login('SYSOP','1234').should == true
+        u.login('SYSOP','4321').should == false
+        u.login('SYSOP2','1234').should == false
+        u.login('user1','4321').should == true
+        u.login('user2','1234').should == true
+      end
     end
   end
-end
 
-describe Loco::Fileheader do
-  before do
-    CStruct.include_dirs=['../loco']
-    CStruct.headers=['bbs.h']
-    CStruct::Crawler.save('structures.yml')
-    Loco::path = '../loco'
-  end
-  it 'should have fileheader class' do
-    Loco::Fileheader.as do |d|
-      d.map do |dd|
-        [dd.filename, dd.owner, dd.isdirectory]
-      end.should == [['a', 'SYSOP', 0]]
+  describe Loco::Fileheader do
+    it 'should have fileheader class' do
+      Loco::Fileheader.as do |d|
+        d.map do |dd|
+          [dd.filename, dd.owner, dd.isdirectory]
+        end.should == [['a', 'SYSOP', 0]]
+      end
     end
   end
-end
 
-describe Loco::Dir_fileheader do
-  before do
-    CStruct.include_dirs=['../loco']
-    CStruct.headers=['bbs.h']
-    CStruct::Crawler.save('structures.yml')
-    Loco::path = '../loco'
-  end
-  it 'should have dir_fileheader class' do
-    Loco::Dir_fileheader.as('a') do |d|
-      d.map do |dd|
-        [dd.filename, dd.owner, dd.title]
-      end.should == [['M.1287734548.A', 'SYSOP', 'post 1']]
+  describe Loco::Dir_fileheader do
+    it 'should have dir_fileheader class' do
+      Loco::Dir_fileheader.as('a') do |d|
+        d.map do |dd|
+          [dd.filename, dd.owner, dd.title]
+        end.should == [['M.1287734548.A', 'SYSOP', 'post 1']]
+      end
     end
   end
+
 end
