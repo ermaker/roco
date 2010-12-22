74e89886cdc7cd7f7df1995eda472f20b30b612c
Minwoo Lee <ermaker@gmail.com>
2010-12-22 14:12:35 +0900

    Refactor

--- spec/loco_spec.rb
+++ spec/loco_spec.rb
@@ -86,10 +86,18 @@ describe Loco do
     loco.articles('a', 5, 2) do |a|
       a.map {|v| [v.title, v.owner, v.visit[usernum], v.read[usernum]]}
     end.should == []
+  end
 
+  it 'should read a article' do
+    loco = Loco.new
+    loco.login('user2','1234').should_not == nil
+    usernum = loco.instance_eval('@usernum')
+
+    loco.articles('a') do |a|
+      a.map {|v| [v.title, v.owner, v.visit[usernum], v.read[usernum]]}
+    end.should == [["d", "user2", false, true], ["c", "user1", false, false], ["b", "user1", true, false], ["a", "user1", false, true], ["post 1", "SYSOP", false, true]]
     article_filename = loco.articles('a', 1, 1) {|a|a[0].filename}
     loco.read('a', article_filename).should == "\261\333\276\264\300\314: user1 (nick1)\n\263\257  \302\245: 2010/12/21 (\310\255) 19:30:09\n\301\246  \270\361: c\n\nc\n"
-
     loco.articles('a') do |a|
       a.map {|v| [v.title, v.owner, v.visit[usernum], v.read[usernum]]}
     end.should == [["d", "user2", false, true], ["c", "user1", false, true], ["b", "user1", true, false], ["a", "user1", false, true], ["post 1", "SYSOP", false, true]]
